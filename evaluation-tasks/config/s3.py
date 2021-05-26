a871e64d6e2c333f287cd8f38b3d0e2d248d3d76
Jordie Shier <jordieshier@gmail.com>
2021-05-26 09:48:15 -0700

    Change S3 config back to defaults

--- evaluation-tasks/config/s3.py
+++ evaluation-tasks/config/s3.py
@@ -7,9 +7,9 @@ S3_CACHE = True
 
 # You should pick a unique handle, since this determine the S3 path
 # (which must be globally unique across all S3 users).
-HANDLE = "jshier"
+HANDLE = "hear"
 S3_BUCKET = f"hear2021-{HANDLE}"
 
 # If this is None, boto will use whatever is in your
 # ~/.aws/config or AWS_DEFAULT_REGION environment variable
-S3_REGION_NAME = "us-west-2"
+S3_REGION_NAME = "eu-central-1"
