fec2f28e861442b5e8d36130aefca54441d732e1
khumairraj <humair444@gmail.com>
2021-07-27 02:34:36 +0530

    remove coughvid in runner

--- heareval/tasks/runner.py
+++ heareval/tasks/runner.py
@@ -9,7 +9,6 @@ from typing import Optional
 
 import click
 
-import heareval.tasks.coughvid as coughvid
 import heareval.tasks.nsynth_pitch as nsynth_pitch
 import heareval.tasks.speech_commands as speech_commands
 import heareval.tasks.dcase2016_task2 as dcase2016_task2
@@ -20,7 +19,6 @@ tasks = {
     "speech_commands": speech_commands,
     "nsynth_pitch": nsynth_pitch,
     "dcase2016_task2": dcase2016_task2,
-    "coughvid": coughvid,
 }
 
 
