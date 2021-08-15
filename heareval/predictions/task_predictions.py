6899cbc5f4727d9d22f84b84f059bf62ccaa446f
Joseph Turian <turian@gmail.com>
2021-08-15 20:49:50 +0200

    mypy

--- heareval/predictions/task_predictions.py
+++ heareval/predictions/task_predictions.py
@@ -50,6 +50,7 @@ class RandomProjectionPrediction(torch.nn.Module):
 
         self.projection = torch.nn.Linear(nfeatures, nlabels)
         torch.nn.init.normal_(self.projection.weight)
+        self.logit_loss: torch.nn.Module
         if prediction_type == "multilabel":
             self.activation: torch.nn.Module = torch.nn.Sigmoid()
             self.logit_loss = torch.nn.BCEWithLogitsLoss()
@@ -84,7 +85,7 @@ class PredictionModel(pl.LightningModule):
         self.label_to_idx = label_to_idx
         self.scores = scores
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         return self.predictor(x)
 
     def training_step(self, batch, batch_idx):
