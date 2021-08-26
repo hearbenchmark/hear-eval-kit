535aa3b910f600397318a11b401b75d1b7b81c68
khumairraj <humair444@gmail.com>
2021-08-27 03:21:06 +0530

    add secret tasks to the all list

--- heareval/tasks/runner.py
+++ heareval/tasks/runner.py
@@ -33,10 +33,12 @@ tasks = {
     "speech_commands": [speech_commands],
     "nsynth_pitch": [nsynth_pitch],
     "dcase2016_task2": [dcase2016_task2],
-    "all": [speech_commands, nsynth_pitch, dcase2016_task2],
+    "all": [speech_commands, nsynth_pitch, dcase2016_task2].extend(
+        secret_tasks.get("all-secrets", [])
+    ),
     # Add the task config for the secrets task if the secret task config was found.
     # Not available for participants
-    **secret_tasks,
+    **{task: module for task, module in secret_tasks.items() if task != "all-secrets"},
 }
 
 
