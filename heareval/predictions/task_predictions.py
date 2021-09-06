3fdf0fcf42a591c03a3d1f658b2449b678782621
Joseph Turian <turian@gmail.com>
2021-09-06 11:53:38 +0200

    Adjust hidden layers

--- heareval/predictions/task_predictions.py
+++ heareval/predictions/task_predictions.py
@@ -48,7 +48,8 @@ from heareval.score import (
 )
 
 PARAM_GRID = {
-    "hidden_layers": [0, 1, 2, 3],
+    "hidden_layers": [0, 1, 2],
+    # "hidden_layers": [0, 1, 2, 3],
     # "hidden_dim": [256, 512, 1024],
     "hidden_dim": [1024, 512],
     "dropout": [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
