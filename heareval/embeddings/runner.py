f4b0f5cb1a0abd6bf9e70a8e08ac05c93997b83e
Joseph Turian <turian@gmail.com>
2021-09-06 08:11:59 +0200

    Remove tree if necessary

--- heareval/embeddings/runner.py
+++ heareval/embeddings/runner.py
@@ -102,6 +102,9 @@ def runner(
         if os.path.exists(done_embeddings):
             continue
 
+        if os.path.exists(embed_task_dir):
+            shutil.rmtree(embed_task_dir)
+
         print(f"Computing embeddings for {task_path.name}")
         start = time.time()
         gpu_max_mem.reset()
