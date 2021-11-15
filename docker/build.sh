9f0d5c5522155f09179ff1ea2854a77b0749f34c
Joseph Turian <turian@gmail.com>
2021-11-15 22:28:43 +0100

    Update dockers

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 docker build -t turian/heareval:latest -f docker/Dockerfile-cuda11.2 . && docker push turian/heareval:latest
+docker build -t turian/heareval:latest38 -f docker/Dockerfile-cuda11.2-python3.8 . && docker push turian/heareval:latest38
+docker build -t turian/heareval:latestmulti -f docker/Dockerfile-cuda11.2-multipy . && docker push turian/heareval:latestmulti
 docker build -t turian/heareval:stable -f docker/Dockerfile-cuda11.2-stable . && docker push turian/heareval:stable
